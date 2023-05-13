387224328f54df5281254e36e2b1ba916e3e6eba
michalprzyl <michal.przyl@gmail.com>
2023-05-13 23:46:39 +0200

    serializers.py change

--- backend/language/serializers.py
+++ backend/language/serializers.py
@@ -9,11 +9,11 @@ class UserRelatedField(serializers.RelatedField):
             'id': instance.id,
             'username': instance.username
         }
+    
     def to_internal_value(self, data):
         return User.objects.get(id=data)
 
 class LanguageSerializer(serializers.ModelSerializer):
-    # user = serializers.CharField(source='user.username', read_only=True)
     user = UserRelatedField(queryset=User.objects.all())
     
     class Meta:
